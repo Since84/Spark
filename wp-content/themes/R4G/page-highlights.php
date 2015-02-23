c6de9c19daa4ecda2501ea48450e230f07869531
Damon <damonjhastins@gmail.com>
2015-02-22 19:30:34 -0800

    2/22

--- wp-content/themes/R4G/page-highlights.php
+++ wp-content/themes/R4G/page-highlights.php
@@ -4,31 +4,20 @@
 	//Get Timber Context. Provides Data to TWIG views
 	$context 		= Timber::get_context();
 	
-	///Page Feature
-	$featureContext	= Timber::get_context();
-	$featureContext['heading'] 		= get_field( 'feature_heading', get_the_ID());
-	$featureContext['text'] 		= get_field( 'feature_text', get_the_ID());
-	$featureContext['link'] 		= get_field( 'feature_link', get_the_ID()); 
-	$featureContext['link_text'] 	= get_field( 'feature_link_text', get_the_ID()); 
-	$featureContext['link_label'] 	= get_field( 'feature_link_label', get_the_ID()); 
-	$featureContext['image'] 		= get_field( 'feature_image', get_the_ID()); 
-
-	$context['feature'] = Timber::compile('/views/components/feature.html.twig', $featureContext);
-
-	/// Papers & Factsheets
-	$papersContext['feed'] 			= 	get_field('image_feed');
-	$paperscontext['spark_class'] 	= 'letters-briefs-petitions';
- 	$context['papers_factsheets'] 	= Timber::compile('/views/components/thumbnail_carousel_gallery.html.twig', $papersContext);
-
+	$galleryContext['feed'] = get_field('highlight_gallery');
+	$galleryContext['slide_template'] = '/views/content/news_slide.html.twig';
+	$context['gallery'] 				= Timber::compile('/views/components/thumbnail_carousel_gallery.html.twig', $galleryContext);
 
  	/// Accomplishments
- 	$accomplishmentsContext['feed'] 			= 	get_field('accomplishments');
+	$accomplishmentsContext['feed'] 			= 	get_field('accomplishments');
+ 	// var_dump($accomplishmentsContext['feed']);
 	$accomplishmentsContext['spark_class'] 		= 'accomplishments';
-	$accomplishmentsContext['slide_template'] 	= '/views/content/news_slide.html.twig';
- 	$context['accomplishments'] 				= Timber::compile('/views/components/thumbnail_carousel_gallery.html.twig', $accomplishmentsContext);
+	$accomplishmentsContext['header'] 			= 'Accomplishments';
+	$accomplishmentsContext['slide_template'] 	= '/views/content/accomplishment_slide.html.twig';
+	$context['accomplishments']					= Timber::compile('/views/components/static_feed.html.twig', $accomplishmentsContext );
 
 	//Display Page using home template 
-	Timber::render('/views/pages/action.html.twig', $context);
+	Timber::render('/views/pages/highlights.html.twig', $context);
 
 	get_footer();
 ?>
