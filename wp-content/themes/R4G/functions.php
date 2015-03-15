6854ec5494212a611a5d7b517b141144b09073e7
Damon <damonjhastins@gmail.com>
2015-03-14 22:51:58 -0700

    Style update

--- wp-content/themes/R4G/functions.php
+++ wp-content/themes/R4G/functions.php
@@ -7,16 +7,16 @@ function theme_enqueue_styles() {
 
     wp_enqueue_script( 'spark_child_site', get_stylesheet_directory_uri()."/script/site.js", array('jquery', 'site') );
 }
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-  register_post_type( 'donation_lightbox',
-    array(
-      'labels' => array(
-        'name' => __( 'lightbox' ),
-        'singular_name' => __( 'lightboxes' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-    )
-  );
-}
+// add_action( 'init', 'create_post_type' );
+// function create_post_type() {
+//   register_post_type( 'donation_lightbox',
+//     array(
+//       'labels' => array(
+//         'name' => __( 'lightbox' ),
+//         'singular_name' => __( 'lightboxes' )
+//       ),
+//       'public' => true,
+//       'has_archive' => true,
+//     )
+//   );
+// }
