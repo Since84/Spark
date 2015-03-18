799328b983b50abfec71ca51a1f25a8a02088d40
Damon <damonjhastins@gmail.com>
2015-03-18 16:05:45 -0700

    Event Detail, Issues

--- wp-content/themes/R4G/script/site.js
+++ wp-content/themes/R4G/script/site.js
@@ -18,6 +18,7 @@
 	  			"click .spark-modal-trigger.donate": "openDonateModal",
 	  			"click .spark-modal-trigger.gallery": "openGalleryFull",
 	  			"click .events .slide": "toggleEvent",
+	  			"click .event-detail.open": "toggleEvent",
 	  			"click .news-feed .post-preview": "openNewsPost",
 	  			"click .video": "playVideo",
 	  			"click .load-more": "loadMore"
